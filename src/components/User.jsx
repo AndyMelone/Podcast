f21b1b20f51adf1f0b5c494ef1f0fb2055813dc7
ncho <ncho@ncho.com>
2024-01-26 04:46:03 +0000

    mise 3

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -17,14 +17,14 @@ const user = [
   {
     photo: "ncho.png",
     nom: "N'cho boni  n'gbesso",
-    poste: "Directrice",
+    poste: "Responsable technique",
     description: "Description du Directeur Financier.",
   },
   {
     photo: "ndri.png",
     nom: "N'DRI KOUAME OTHNIEL JUNIOR HERWAN",
     poste: "Directeur des ressources humaines",
-    description: "N'DRI KOUAME OTHNIEL JUNIOR HERWAN, véritable prodige de l'école ivoirienne, a vu le jour le 15 août 1984. Son parcours académique débute au prestigieux lycée classique d'Abidjan, où sa brillance se révèle déjà.Après avoir triomphé au baccalauréat, Othniel Junior Herwan poursuit ses études en management et ressources humaines à l'illustre université canadienne d'Alberta, où il décroche avec éclat un Master dans cette discipline prestigieuse. Son talent exceptionnel ne passe pas inaperçu, et il fait ses premiers pas professionnels au sein de la banque d'affaires Standard Chartered, où il accumule six années d'expérience enrichissante.Sa renommée s'accroît encore davantage lorsqu'il est recruté par la Banque mondiale en tant que conseiller technique, chargé de la politique managériale du numérique et des ressources humaines. En janvier 2014, décidant de suivre sa voie, il présente sa démission et fait son retour triomphant dans son pays natal, la Côte d'Ivoire. À son retour, il intègre Humanbot en tant que Directeur des ressources humaines, un poste de haute responsabilité qu'il occupe avec éclat depuis lors, inscrivant ainsi son nom parmi les figures éminentes du monde professionnel ivoirien.",
+    description: "N'DRI KOUAME OTHNIEL JUNIOR HERWAN, véritable prodige de l'école ivoirienne, a vu le jour le 15 août 1984. Son parcours académique débute au prestigieux lycée classique d'Abidjan, où sa brillance se révèle déjà.Après avoir triomphé au baccalauréat, Othniel Junior Herwan poursuit ses études en management et ressources humaines à l'illustre université canadienne d'Alberta, où il décroche avec éclat un Master dans cette discipline prestigieuse. Son talent exceptionnel ne passe pas inaperçu, et il fait ses premiers pas professionnels au sein de la banque d'affaires Standard Chartered, où il accumule six années d'expérience enrichissante.Sa renommée s'accroît encore davantage lorsqu'il est recruté par la Banque mondiale en tant que conseiller technique, chargé de la politique managériale du numérique et des ressources humaines. En janvier 2014, décidant de suivre sa voie, il présente sa démission et fait son retour triomphant dans son pays natal, la Côte d'Ivoire. À son retour, il intègre HumanBot en tant que Directeur des ressources humaines, un poste de haute responsabilité qu'il occupe avec éclat depuis lors, inscrivant ainsi son nom parmi les figures éminentes du monde professionnel ivoirien.",
   },
   {
     photo: "melone.png",
@@ -37,7 +37,7 @@ const user = [
     photo: "meite.png",
     nom: "Meite Lassina",
     poste: "Directeur Financier",
-    description: "Description du Directeur Financier.",
+    description: "Directeur financier chez HumanBot, je suis chargé de superviser l'ensemble des activités financières. Mes responsabilités englobent la gestion stratégique des finances, la planification budgétaire, l'analyse financière approfondie et la communication transparente avec les parties prenantes externes, y compris les investisseurs. En tant que gardien de la stabilité financière, je joue le rôle central dans la prise de décisions éclairées pour atténuer les risques, maintenir la conformité réglementaire, et favoriser la croissance durable de l'entreprise. Je dirige également les équipes financières, assurant l'intégrité des rapports financiers et la gestion efficace de la trésorerie, tout en contribuant activement à la planification stratégique globale de l'organisation.",
   },
   
   
